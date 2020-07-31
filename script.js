767a7cae14265d34e920e1d85b35095d9bcaede8
[InnCreator] <[engbethany4@gmail.com]>
2020-07-31 14:06:36 +0300

    starting js

---
+++ script.js
@@ -0,0 +1,8 @@
+const username = document.getElementsByName('username')
+const password = document.getElementsByName('password')
+const submit = document.getElementsById('submit')
+
+var usernames = (['Sandra', 'sandra101'], ['Susan', 'susan102'], ['Brian', 'brian103'], ['Brandon', 'brandon104'],
+    ['Rachael', 'rachael105'], ['George', 'george106'], ['Paul', 'paul107'], ['Patricia', 'patricia108'])
+
+let username = () => alert("Hello!");
